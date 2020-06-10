be3a97d0306ed4b1acceec138c7fc7f0107daa65
shingler <singlerwong@qq.com>
2020-06-10 18:47:39 +0800

    ICP备案号

--- gfweb/settings/develop.py
+++ gfweb/settings/develop.py
@@ -23,7 +23,7 @@ SECRET_KEY = '*74sufjll3c(oc(=_9#0uy(%nb-o651f@ct0g1s4m2ab*v@@f)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
